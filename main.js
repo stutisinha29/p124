8cb8b4fa99d0987b1bb258271cb33fbfacf2c686
stutisinha29 <88232935+stutisinha29@users.noreply.github.com>
2022-10-07 16:25:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 400);
+    canvas.position(560,100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#2fe0e0');
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results); 
+    }} 
